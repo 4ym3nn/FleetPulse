03cdc4bd9d27067b892c1e60f1c40f4341113e36
Aymen-drid <aymnehocam@gmail.com>
2025-11-16 16:29:48 +0100

    feat(api): update API routes

--- trackiny-backend/trackiny/routes/api.php
+++ trackiny-backend/trackiny/routes/api.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransportController;
+
 
 Route::post('/login',[UserController::class,'login']);
 Route::post('/register',[UserController::class,'register']);
 Route::post('/logout',[UserController::class,'logout']);
+Route::post('/transport/add-shipment',[TransportController::class,'addShipment']);
+
 
 
