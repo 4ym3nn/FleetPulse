6b10f2c10ec234eb062984b67e2da52923695e75
Aymen-drid <aymnehocam@gmail.com>
2025-11-17 17:20:53 +0100

    Update bootstrap/app.php configuration

--- trackiny-backend/trackiny/bootstrap/app.php
+++ trackiny-backend/trackiny/bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\EnsureTokenIsValid;
+use App\Http\Middleware\ForceJsonResponse;
+
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
 
@@ -26,8 +29,15 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(EnsureTokenIsValid::class);
+        $middleware->append(ForceJsonResponse::class);
+
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, Request $request) {
+            if ($request->wantsJson()) {
+                return response()->json(['message' => 'Not Found!'], 404);
+            }
+        });
     })->create();
