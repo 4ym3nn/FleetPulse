3c7c3f7b6a2744d4a43e2a51ba9b682f210d7c70
Aymen-drid <aymnehocam@gmail.com>
2025-11-19 19:16:38 +0100

    Update User model: roles/relations

--- trackiny-backend/trackiny/app/Models/User.php
+++ trackiny-backend/trackiny/app/Models/User.php
@@ -3,15 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use App\Model\Transport;
-use App\Model\Company;
+use App\Models\Transport;
+use App\Models\Company;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable , HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
